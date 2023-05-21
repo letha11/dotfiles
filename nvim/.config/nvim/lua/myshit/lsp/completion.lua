72b1c3ff7a3870d4f89568e0dfd74166dca841f5
letha11 <ibkaanhar1@gmail.com>
2023-05-21 10:52:05 +0700

    add

--- nvim/.config/nvim/lua/myshit/lsp/completion.lua
+++ nvim/.config/nvim/lua/myshit/lsp/completion.lua
@@ -1,6 +1,28 @@
 local cmp = require('cmp')
 local luasnip = require('luasnip')
 
+local unlinkgrp = vim.api.nvim_create_augroup(
+  'UnlinkSnippetOnModeChange',
+  { clear = true }
+)
+
+vim.api.nvim_create_autocmd('ModeChanged', {
+  group = unlinkgrp,
+  pattern = { 's:n', 'i:*' },
+  desc = 'Forget the current snippet when leaving the insert mode',
+  callback = function(evt)
+    -- If we have n active nodes, n - 1 will still remain after a `unlink_current()` call.
+    -- We unlink all of them by wrapping the calls in a loop.
+    while true do
+      if luasnip.session and luasnip.session.current_nodes[evt.buf] and not luasnip.session.jump_active then
+        luasnip.unlink_current()
+      else
+        break
+      end
+    end
+  end,
+})
+
 -- snippets
 luasnip.config.setup {}
 require("luasnip.loaders.from_snipmate").lazy_load()
