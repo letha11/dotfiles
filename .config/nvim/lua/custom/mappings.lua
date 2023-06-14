54c19b08c429206732b2166a55de7a6ecfc80a29
letha11 <ibkaanhar1@gmail.com>
2023-06-14 10:56:33 +0700

    better readme

--- .config/nvim/lua/custom/mappings.lua
+++ .config/nvim/lua/custom/mappings.lua
@@ -2,79 +2,78 @@
 local M = {}
 
 M.general = {
-  -- n = {
-  --   [";"] = { ":", "enter command mode", opts = { nowait = true } },
-  -- },
+	-- n = {
+	--   ["F"] = { ":", "enter command mode", opts = { nowait = true } },
+	-- },
 }
 
 M.nvimtree = {
-  plugin = true,
+	plugin = true,
 
-  n = {
-    ["<A-1>"] = { ":NvimTreeToggle<CR>", "Toggle NvimTree" },
-  },
+	n = {
+		["<A-1>"] = { ":NvimTreeToggle<CR>", "Toggle NvimTree" },
+	},
 }
 
 M.lspconfig = {
-  n = {
-    ["<C-A-l>"] = {
-      function()
-        vim.lsp.buf.format { async = true }
-      end,
-      "LSP formatting",
-    },
-  },
+	n = {
+		["<C-A-l>"] = {
+			function()
+				vim.lsp.buf.format({ async = true })
+			end,
+			"LSP formatting",
+		},
+	},
 }
 
 M.hop = {
-  plugin = true,
-  n = {
-    ["<S-f>"] = { ":HopChar1<CR>", "Hop Word" },
-  },
-  v = {
-    ["F"] = { "<cmd>HopChar1<CR>", "Hop Word" },
-  },
+	n = {
+		["F"] = { "<cmd>HopChar1<CR>", "Hop Word" },
+	},
+	v = {
+		["F"] = { "<cmd>HopChar1<CR>", "Hop Word" },
+	},
 }
 
 M.tabufline = {
-  -- cycle through buffers
-  n = {
-    ["<S-l>"] = {
-      function()
-        require("nvchad_ui.tabufline").tabuflineNext()
-      end,
-      "Goto next buffer",
-    },
+	-- cycle through buffers
+	n = {
+		["<S-l>"] = {
+			function()
+				require("nvchad_ui.tabufline").tabuflineNext()
+			end,
+			"Goto next buffer",
+		},
 
-    ["<S-h>"] = {
-      function()
-        require("nvchad_ui.tabufline").tabuflinePrev()
-      end,
-      "Goto prev buffer",
-    },
+		["<S-h>"] = {
+			function()
+				require("nvchad_ui.tabufline").tabuflinePrev()
+			end,
+			"Goto prev buffer",
+		},
 
-    -- close buffer + hide terminal buffer
-    ["<leader>q"] = {
-      function()
-        require("nvchad_ui.tabufline").close_buffer()
-      end,
-      "Close buffer",
-    },
-  },
+		-- close buffer + hide terminal buffer
+		["<leader>q"] = {
+			function()
+				require("nvchad_ui.tabufline").close_buffer()
+			end,
+			"Close buffer",
+		},
+	},
 }
 
 M.telescope = {
-  plugin = true,
-  n = {
-    ["<C-p>"] = { "<cmd> Telescope find_files <CR>", "Find files" },
-  },
+	plugin = true,
+	n = {
+		["<C-p>"] = { "<cmd> Telescope find_files <CR>", "Find files" },
+	},
 }
 
 M.disabled = {
-  n = {
-    ["<C-n>"] = false,
-    ["<leader>e"] = false,
-  },
+	n = {
+		["<C-n>"] = false,
+		["<leader>e"] = false,
+	},
 }
 -- more keybinds!
 
--- readme.md
+++ readme.md
@@ -1,5 +1,20 @@
 # My Dotfile
 
+## Application requirement to use this dotfiles
+### Arch
+```
+$ yay/pacman -S base-devel brightnessctl ctags dunst exa figma-linux fvm-bin flameshot alacritty neovim nvm ntfs-3g perl nginx php php-fpm composer apache mariadb rar scrcpy stow syncthing rustup ripgrep tmux zsh 
+```
+
+#### Laptop
+If you are using laptop you should install `tlp` package or [auto-cpufreq](https://github.com/AdnanHodzic/auto-cpufreq) for better battery life
+
+#### PHP Development
+I have an script that will start `nginx`, `mariadb`, and `php-fpm` from the systemctl, it placed in `scripts/run-lemp.sh`. if you want to use `apache` you can just run the command
+```
+$ sudo systemctl start httpd.service
+```
+because we already installed the `apache` package.
 
 ## NOTE
 You need to install **NvChad** first in order to use the `nvim` configuration.
